5bd3670129ef5632a4de46e4cd4af5bce11645de
Eduardo Ribeiro Pimenta <eduardorpimentaa@gmail.com>
2023-12-06 16:27:34 -0300

    ordem

--- src/controller/PostagemController.ts
+++ src/controller/PostagemController.ts
@@ -64,12 +64,12 @@ async getpostPlayer(req: Request, res: Response) {
 
   
     if( !isNaN(perPageNumber) && !isNaN(pagenumber)){
-      postagemResponse = await postagemRepository.find({relations: { dono_id: true, time: true }, take: perPageNumber, skip: skip, where:{tipo: tipo} }) 
+      postagemResponse = await postagemRepository.find({relations: { dono_id: true, time: true }, take: perPageNumber, skip: skip, where:{tipo: tipo}, order: { id: "DESC" } }) 
   
     
       
     }else{
-      postagemResponse = await postagemRepository.find({relations: { dono_id: true, time: true }, where: {tipo: tipo}  })
+      postagemResponse = await postagemRepository.find({relations: { dono_id: true, time: true }, where: {tipo: tipo}, order: { id: "DESC" }  })
     }
 
     let postCount = await postagemRepository.count({where: {tipo: tipo}})
